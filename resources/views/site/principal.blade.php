b69078ced5e10e2a91dd1606bbc45910339134db
Hiury Lima <limahiury6@gmail.com>
2025-05-01 16:21:19 -0300

    Blade components (@component)

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -4,7 +4,6 @@
 
 @section('conteudo')
     <div class="conteudo-destaque">
-
         <div class="esquerda">
             <div class="informacoes">
                 <h1>Sistema Super Gestão</h1>
@@ -28,24 +27,8 @@
             <div class="contato">
                 <h1>Contato</h1>
                 <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.<p>
-                <form>
-                    <input type="text" placeholder="Nome" class="borda-branca">
-                    <br>
-                    <input type="text" placeholder="Telefone" class="borda-branca">
-                    <br>
-                    <input type="text" placeholder="E-mail" class="borda-branca">
-                    <br>
-                    <select class="borda-branca">
-                        <option value="">Qual o motivo do contato?</option>
-                        <option value="">Dúvida</option>
-                        <option value="">Elogio</option>
-                        <option value="">Reclamação</option>
-                    </select>
-                    <br>
-                    <textarea class="borda-branca">Preencha aqui a sua mensagem</textarea>
-                    <br>
-                    <button type="submit" class="borda-branca">ENVIAR</button>
-                </form>
+                @component('site.layouts._components.form_contato')
+                @endcomponent
             </div>
         </div>
     </div>
